5c6590e5ebf0d57dd15fe8cd4eaaa70b3a106249
S.M. Ataur Rahman <dev.ataur.rahman@gmail.com>
2021-04-04 13:27:47 +0600

    very ugly way to define sql functions, but it works, so...

---
+++ src/Query/Parts/SqlFunction.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace Falgun\Typo\Query\Parts;
+
+use Falgun\Typo\Interfaces\ColumnLikeInterface;
+
+final class SqlFunction implements ColumnLikeInterface
+{
+
+    private string $functionName;
+    private ColumnLikeInterface $mainColumn;
+    private ?string $columnAlias;
+
+    private function __construct(string $functionName, ColumnLikeInterface $mainColumn)
+    {
+        $this->functionName = $functionName;
+        $this->mainColumn = $mainColumn;
+        $this->columnAlias = null;
+    }
+
+    public static function call(
+        string $functionName,
+        ColumnLikeInterface $mainColumn
+    ): SqlFunction
+    {
+        return new static($functionName, $mainColumn);
+    }
+
+    public function as(string $alias): ColumnLikeInterface
+    {
+        $object = clone $this;
+        $object->columnAlias = $alias;
+
+        return $object;
+    }
+
+    public function getBindValues(): array
+    {
+        return $this->mainColumn->getBindValues();
+    }
+
+    public function getSQL(): string
+    {
+        $sql = $this->functionName . '(' . $this->mainColumn->getSQL() . ')';
+
+        if (isset($this->columnAlias)) {
+            $sql .= ' as ' . $this->columnAlias;
+        }
+
+        return $sql;
+    }
+}
